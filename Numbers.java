40cd93193226b2ef3fa1cba17dae6ab3bf614fd9
Mohammad Ashif <mohammadashif510@gmail.com>
2024-10-28 11:36:04 +0600

    array in java

---
+++ Numbers.java
@@ -0,0 +1,13 @@
+public class Numbers {
+    void displayText(){
+        System.out.println("Everything is ok");
+    }
+    public static void main(String[] args) {
+        int [] newNumber = {11, 22, 33, 44, 55};
+        Numbers func = new Numbers();
+        for(int i = 0; i<newNumber.length; i++){
+            System.out.println("Number is : "+ newNumber[i]);
+            func.displayText();
+        }
+    }
+}
